719b41c621247b8ceaaf36a607df1dd6acb9aa71
ChristianDMelendez <60398695+ChristianDMelendez@users.noreply.github.com>
2025-06-17 06:57:39 -0500

    Update blob.js

--- blob.js
+++ blob.js
@@ -3,22 +3,26 @@ let clock = new THREE.Clock();
 
 function initVisualizer(audioFeatureValue = 0.5) {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-  renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("blob-canvas"), antialias: true });
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById("blob-canvas"),
+    antialias: true
+  });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   uniforms = {
     u_time: { value: 0.0 },
-    u_intensity: { value: audioFeatureValue },
+    u_intensity: { value: audioFeatureValue }
   };
 
   const loader = new THREE.FileLoader();
   let vertexShader, fragmentShader;
 
-  loader.load('vertex.glsl.txt', function (vert) {
+  loader.load('vertex.txt', function (vert) {
     vertexShader = vert;
-    loader.load('fragment.glsl.txt', function (frag) {
+    loader.load('fragment.txt', function (frag) {
       fragmentShader = frag;
+
       geometry = new THREE.IcosahedronGeometry(2, 100);
       material = new THREE.ShaderMaterial({
         uniforms: uniforms,
@@ -26,6 +30,7 @@ function initVisualizer(audioFeatureValue = 0.5) {
         fragmentShader: fragmentShader,
         wireframe: true
       });
+
       mesh = new THREE.Mesh(geometry, material);
       scene.add(mesh);
       camera.position.z = 5;
